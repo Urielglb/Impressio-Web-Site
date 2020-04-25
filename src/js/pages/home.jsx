78224a7a682865d4ab1adea414e8ed79213f3335
Uriel García Luna Bobadilla <uriel_06@ciencias.unam.mx>
2020-04-25 17:31:43 -0500

    Cubrebocas

--- src/js/pages/home.jsx
+++ src/js/pages/home.jsx
@@ -3,6 +3,7 @@ import content from '../slider/sliderContent';
 import Slider from '../slider/sliderForm'
 import Header from '../header';
 import ContactUs from '../contactUs'
+import promotion from '../../assets/img/tapabocas.jpeg'
 
 
 const Home = ()=>{
@@ -11,6 +12,8 @@ const Home = ()=>{
          <Header></Header>
          <Slider imgs={content}></Slider>
          {/* <ContactUs></ContactUs> */}
+         <h2>Promoción de cubrebocas y caretas</h2>
+         <img src={promotion} alt="cubre bocas y caretas" id="masks"></img>
     </div>
    
     );
